7907bc625263e4f6bc9411b39aebbe697dd162cd
杨捷 <myworld4059@163.com>
2014-09-13 16:36:19 +0800

    Create this.js

---
+++ app/core/this.js
@@ -0,0 +1,30 @@
+/**
+ * 核心功能扩展
+ * 
+ */
+
+
+
+/**
+ * 扩展核心功能
+ *
+ * 【注意】命名如有冲突，新添加的扩展功能将覆盖系统核心功能！
+ */
+
+
+/* //示例核心功能扩展
+exports.end = function(){
+
+	// 在此处可以使用系统的核心功能
+
+
+	log('response!');
+
+    this.render('end by extend !!!');
+
+    //this.response.end('end by extend !!!');
+
+}
+
+*/
+
